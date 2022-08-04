c00ce2af2beafd408936c0c481d10ed20256804a
kafreekaboom <randunu1483@gmail.com>
2022-08-04 18:29:20 +0530

    WBS and Gantt added dates
    added dates and timelines. please check

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -501,7 +501,12 @@
             <input type="submit" value="subscirbe" class="btn">
         </form>
     </div>
-
+    <div class="content">
+        <h1 class="heading">MAP</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7921.343540662942!2d79.86574332841269!3d6.92977905658505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae253d10f7a7003%3A0x320b2e4d32d3838d!2sColombo!5e0!3m2!1sen!2slk!4v1644135377307!5m2!1sen!2slk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    
+    </div>
+   
 </section>
 
 
