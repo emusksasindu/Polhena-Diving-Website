14170cd9a58017209a4025c2b554dd51c18294a1
Sasidu Sandakalum <84926805+emusksasindu@users.noreply.github.com>
2022-09-04 00:16:00 +0530

    Update guest.php

--- app/Models/guest.php
+++ app/Models/guest.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class guest extends Model
 {
     use HasFactory;
+
+
+    public function chats(): HasMany
+    {
+        return $this->hasMany(chat::class);
+    }
 }
