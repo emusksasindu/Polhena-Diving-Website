8704553dab146d545e6077d1985f38cf78032809
Sasidu Sandakalum <84926805+emusksasindu@users.noreply.github.com>
2022-09-04 00:17:34 +0530

    Update chat.php

--- app/Models/chat.php
+++ app/Models/chat.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class chat extends Model
 {
     use HasFactory;
+
+    public function guest(): BelongsTo
+    {
+        return $this->belongsTo(guest::class);
+    }
 }
