12948cc28c4d134e4e1f99399d9590f43b8522c8
kafreekaboom <89536837+kafreekaboom@users.noreply.github.com>
2022-09-05 16:02:02 +0530

    Update home
    home
    1. updated title
    2. Deleted some quick links
    3. added social links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,10 +6,8 @@
 
         <div class="box">
             <h3>quick links</h3>
-            <a href="#home">home</a>
-            <a href="#about">about</a>
             <a href="#shop">shop</a>
-            <a href="#packages">packages</a>
+            <a href="#packages">Services</a>
             <a href="#reviews">reviews</a>
             <a href="#blogs">blogs</a>
         </div>
@@ -18,27 +16,25 @@
             <h3>extra links</h3>
             <a href="#">my account</a>
             <a href="#">my order</a>
-            <a href="#">my wishlist</a>
-            <a href="#">ask questions</a>
-            <a href="#">terms of use</a>
-            <a href="#">privacy policy</a>
+
+            
         </div>
 
         <div class="box">
             <h3>contact info</h3>
-            <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
-            <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
-            <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
-            <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +94 112 112 112 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +94 715 985 079 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> info@polhenadiving.com </a>
+            <a href="#"> <i class="fas fa-map"></i> Beach Road, Madiha, Matara, Sri Lanka </a>
         </div>
 
         <div class="box">
             <h3>follow us</h3>
-            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
-            <a href="#"> <i class="fab fa-github"></i> github </a>
+            <a href="https://www.facebook.com/polhenadiving"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="https://www.twitter.com/polhenadiving"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="https://www.instagram.com/polhenadiving"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="https://www.linkedin.com/company/26637691"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="#https://github.com/emusksasindu/Polhena-Diving-Website"> <i class="fab fa-github"></i> github </a>
         </div>
 
     </div>
