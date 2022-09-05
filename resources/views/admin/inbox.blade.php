abedc1cf7de6b0c2b1b37b674537134e4f59d56e
Sasidu Sandakalum <84926805+emusksasindu@users.noreply.github.com>
2022-09-05 14:03:08 +0530

    update

--- resources/views/admin/inbox.blade.php
+++ resources/views/admin/inbox.blade.php
@@ -6,217 +6,101 @@
                 <div class="toggle">
                     <ion-icon name="menu-outline"></ion-icon>
                 </div>
-                <div class="user">
-                    <img src="images/customer01.jpg" alt="">
-                </div>
+            
             </div>
 
-            <!-- ======================= Cards ================== -->
-            <div class="cardBox">
-                <div class="card">
-                    <div>
-                        <div class="numbers">1,504</div>
-                        <div class="cardName">Daily Views</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="eye-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">80</div>
-                        <div class="cardName">Sales</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="cart-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">284</div>
-                        <div class="cardName">Comments</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="chatbubbles-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">$7,842</div>
-                        <div class="cardName">Earning</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="cash-outline"></ion-icon>
-                    </div>
-                </div>
-            </div>
-
-            <!-- ================ Order Details List ================= -->
+            <!-- ======================= Message Cards list ================== -->
             <div class="details">
                 <div class="recentOrders">
+                    <h2 class="h2">Inbox</h2>
                     <div class="cardHeader">
-                        <h2>Recent Orders</h2>
-                        <a href="#" class="btn">View All</a>
+                       
+
+                        <div class="inboxCard">
+                            <div class="card">
+                                <div>
+                                    <div class="numbers">Name</div>
+                                    <div class="cardName">Message</div>
+                                </div>
+            
+                                <div class="iconBx">
+                                    <ion-icon name="person-outline"></ion-icon>
+                                </div>
+
+                               <span class="d-flex flex-row-reverse"> <div class="iconBx">
+                                <ion-icon name="trash-outline"></ion-icon>
+                            </div>
+                        </span>
+                            </div>
+
+                            <div class="gap"></div>
+
+                            <div class="card">
+                                <div>
+                                    <div class="numbers">Name</div>
+                                    <div class="cardName">Message</div>
+                                </div>
+            
+                                <span class="d-flex flex-row-reverse"> <div class="iconBx">
+                                    <ion-icon name="trash-outline"></ion-icon>
+                                </div>
+                            </div>
+
+                            <div class="gap"></div>
+
+                            <div class="card">
+                                <div>
+                                    <div class="numbers">Name</div>
+                                    <div class="cardName">Message</div>
+                                </div>
+            
+                                <span class="d-flex flex-row-reverse"> <div class="iconBx">
+                                    <ion-icon name="trash-outline"></ion-icon>
+                                </div>
+                            </div>
+<div class="gap"></div>
+                            
+                            <div class="card">
+                                <div>
+                                    <div class="numbers">Name</div>
+                                    <div class="cardName">Message</div>
+                                </div>
+            
+                                <span class="d-flex flex-row-reverse"> <div class="iconBx">
+                                    <ion-icon name="trash-outline"></ion-icon>
+                                </div>
+                            </div>
+                            <div class="gap"></div>
+                            
+                            <div class="card">
+                                <div>
+                                    <div class="numbers">Name</div>
+                                    <div class="cardName">Message</div>
+                                </div>
+            
+                                <span class="d-flex flex-row-reverse"> <div class="iconBx">
+                                <ion-icon name="trash-outline"></ion-icon>
+                            </div>
+                            </div>
+                            <div class="gap"></div>
+                            
+                            <div class="card">
+                                <div>
+                                    <div class="numbers">Name</div>
+                                    <div class="cardName">Message</div>
+                                </div>
+            
+                                <span class="d-flex flex-row-reverse"> <div class="iconBx">
+                                <ion-icon name="trash-outline"></ion-icon>
+                            </div>
+                            </div>
+            
+                        </div>
                     </div>
-
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>Name</td>
-                                <td>Price</td>
-                                <td>Payment</td>
-                                <td>Status</td>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr>
-                                <td>Star Refrigerator</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status delivered">Delivered</span></td>
-                            </tr>
-
-                            <tr>
-                                <td>Dell Laptop</td>
-                                <td>$110</td>
-                                <td>Due</td>
-                                <td><span class="status pending">Pending</span></td>
-                            </tr>
-
-                            <tr>
-                                <td>Apple Watch</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status return">Return</span></td>
-                            </tr>
-
-                            <tr>
-                                <td>Addidas Shoes</td>
-                                <td>$620</td>
-                                <td>Due</td>
-                                <td><span class="status inProgress">In Progress</span></td>
-                            </tr>
-
-                            <tr>
-                                <td>Star Refrigerator</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status delivered">Delivered</span></td>
-                            </tr>
-
-                            <tr>
-                                <td>Dell Laptop</td>
-                                <td>$110</td>
-                                <td>Due</td>
-                                <td><span class="status pending">Pending</span></td>
-                            </tr>
-
-                            <tr>
-                                <td>Apple Watch</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status return">Return</span></td>
-                            </tr>
-
-                            <tr>
-                                <td>Addidas Shoes</td>
-                                <td>$620</td>
-                                <td>Due</td>
-                                <td><span class="status inProgress">In Progress</span></td>
-                            </tr>
-                        </tbody>
-                    </table>
                 </div>
+           
 
-                <!-- ================= New Customers ================ -->
-                <div class="recentCustomers">
-                    <div class="cardHeader">
-                        <h2>Recent Customers</h2>
-                    </div>
-
-                    <table>
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer02.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer01.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer02.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer01.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer02.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer01.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer01.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="images/customer02.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+          
         </div>
     </div>
 
--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -19,7 +19,7 @@
                 <div class="recentOrders">
                    
                     <div class="cardHeader">
-                        <h2>Latest Services</h2>
+                        <h2>Latest Orders</h2>
                         <a href="#" class="btn">View All</a>
                         
                     </div>
@@ -36,9 +36,13 @@
                                 
                                 <td>ID</td>
                                 <td>Name</td>
-                                <td>Description</td>
-                                <td>Price</td>
-                                <td>Cost</td>
+                                <td>Address</td>
+                                <td>Date</td>
+                                <td>Subtotal</td>
+                                <td>Discount</td>
+                                <td>Total</td>
+                                <td>Contact No</td>
+                                <td>Status</td>
 
                             </tr>
                         </thead>
@@ -46,82 +50,98 @@
                         <tbody>
                             <tr>
                                 <td>1</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
-
+                                <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             
                             </tr>
 
                             <tr>
                                 <td>2</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
-
+                                <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             </tr>
 
                             <tr>
                                 <td>3</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
-
+                                <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             </tr>
 
                             <tr>
                                 <td>4</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
-
+                                <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             </tr>
 
                             <tr>
                                 <td>5</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
-
+                                <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             </tr>
 
                             <tr>
                                 <td>6</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
-
+                                <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             </tr>
 
                             <tr>
                                 <td>7</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
-
+                                <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             </tr>
 
                             <tr>
-                                <td>8</td>
-                                <td>Discover Scuba Diving</td>
-                                <td>Scuba diving for beginners</td>
-                                <td>Rs.136000.00</td>
-                                <td>Rs.80000.00</td>
-                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
+                            <td>jane Doe</td>
+                                <td>Sri Lanka</td>
+                                <td>2022/09/05</td>
+                                <td>Rs.150000.00</td>
+                                <td>Rs.50000.00</td>
+                                <td>Rs.100000.00</td>
+                                <td>0112689456</td>
+                                <td><span class="status inProgress">In Progress</span></td>
                             </tr>
                         </tbody>
                     </table>
