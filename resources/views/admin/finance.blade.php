1a51bd2ed56c98129f2b93765a4c4c751f1cfa51
Sasidu Sandakalum <84926805+emusksasindu@users.noreply.github.com>
2022-09-08 21:22:24 +0530

    update admin panel until part of chat

--- resources/views/admin/finance.blade.php
+++ resources/views/admin/finance.blade.php
@@ -58,14 +58,181 @@
                 
                 </div>
   <!-- ======================= Purchased Products ================== -->
+            <div class="cardFinance">
                 <div class="card">
                     <div>
                         <div class="numbers">Purchased Products</div>
-                       
+                        <div class="input-group">
+                            <div class="form-outline">
+                              <input type="search" id="form1" class="form-control" placeholder="Search"/>
+                            </div>
+                            
+                        </div>
+                        <div class="gap"></div>
+                        <div class="recentOrders table-wrapper-scroll-y my-custom-scrollbar">
+        
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <td>Name</td>
+                                        <td>Price</td>
+                                        <td>Payment</td>
+                                        <td>Status</td>
+                                    </tr>
+                                </thead>
+        
+                                <tbody>
+                                    <tr>
+                                        <td>Star Refrigerator</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status delivered">Delivered</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Dell Laptop</td>
+                                        <td>$110</td>
+                                        <td>Due</td>
+                                        <td><span class="status pending">Pending</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Apple Watch</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status return">Return</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Addidas Shoes</td>
+                                        <td>$620</td>
+                                        <td>Due</td>
+                                        <td><span class="status inProgress">In Progress</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Star Refrigerator</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status delivered">Delivered</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Dell Laptop</td>
+                                        <td>$110</td>
+                                        <td>Due</td>
+                                        <td><span class="status pending">Pending</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Apple Watch</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status return">Return</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Addidas Shoes</td>
+                                        <td>$620</td>
+                                        <td>Due</td>
+                                        <td><span class="status inProgress">In Progress</span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
 
                     
                 </div>
+
+ <!-- ======================= Purchased Services ================== -->
+
+                <div class="card">
+                    <div>
+                        <div class="numbers">Purchased Services</div>
+                        <div class="input-group">
+                            <div class="form-outline">
+                              <input type="search" id="form1" class="form-control" placeholder="Search"/>
+                            </div>
+                            
+                        </div>
+                        <div class="gap"></div>
+                        <div class="recentOrders table-wrapper-scroll-y my-custom-scrollbar">
+        
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <td>Name</td>
+                                        <td>Price</td>
+                                        <td>Payment</td>
+                                        <td>Status</td>
+                                    </tr>
+                                </thead>
+        
+                                <tbody>
+                                    <tr>
+                                        <td>Star Refrigerator</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status delivered">Delivered</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Dell Laptop</td>
+                                        <td>$110</td>
+                                        <td>Due</td>
+                                        <td><span class="status pending">Pending</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Apple Watch</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status return">Return</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Addidas Shoes</td>
+                                        <td>$620</td>
+                                        <td>Due</td>
+                                        <td><span class="status inProgress">In Progress</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Star Refrigerator</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status delivered">Delivered</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Dell Laptop</td>
+                                        <td>$110</td>
+                                        <td>Due</td>
+                                        <td><span class="status pending">Pending</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Apple Watch</td>
+                                        <td>$1200</td>
+                                        <td>Paid</td>
+                                        <td><span class="status return">Return</span></td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Addidas Shoes</td>
+                                        <td>$620</td>
+                                        <td>Due</td>
+                                        <td><span class="status inProgress">In Progress</span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
 
             </div>
 
