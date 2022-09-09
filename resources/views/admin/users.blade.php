aa74c82f2ed1083973edd1c09d69035454287986
kafreekaboom <89536837+kafreekaboom@users.noreply.github.com>
2022-09-09 18:58:33 +0530

    blogs and manage users UI changes

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -36,68 +36,78 @@
                                 
                             </div>
                             <tr>
+                                <td>ID</td>
                                 <td>Name</td>
-                                <td>Price</td>
-                                <td>Payment</td>
+                                <td>Type</td>
                                 <td>Status</td>
+                                <td>Edit/Delete</td>
+                                
                             </tr>
                         </thead>
 
                         <tbody>
                             <tr>
-                                <td>Star Refrigerator</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status delivered">Delivered</span></td>
+                                <td>1</td>
+                                <td>Sasindu</td>
+                                <td>Admin</td>
+                                <td><span class="status delivered">Active</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
 
                             <tr>
-                                <td>Dell Laptop</td>
-                                <td>$110</td>
-                                <td>Due</td>
-                                <td><span class="status pending">Pending</span></td>
+                                <td>2</td>
+                                <td>Randunu</td>
+                                <td>User</td>
+                                <td><span class="status return">disabled</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
 
                             <tr>
-                                <td>Apple Watch</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status return">Return</span></td>
+                            <td>3</td>
+                                <td>Randunu</td>
+                                <td>User</td>
+                                <td><span class="status return">disabled</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
 
                             <tr>
-                                <td>Addidas Shoes</td>
-                                <td>$620</td>
-                                <td>Due</td>
-                                <td><span class="status inProgress">In Progress</span></td>
+                            <td>4</td>
+                                <td>Sasindu</td>
+                                <td>Admin</td>
+                                <td><span class="status delivered">Active</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
 
                             <tr>
-                                <td>Star Refrigerator</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status delivered">Delivered</span></td>
+                            <td>5</td>
+                                <td>Sasindu</td>
+                                <td>Admin</td>
+                                <td><span class="status delivered">Active</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
 
                             <tr>
-                                <td>Dell Laptop</td>
-                                <td>$110</td>
-                                <td>Due</td>
-                                <td><span class="status pending">Pending</span></td>
+                            <td>6</td>
+                                <td>Sasindu</td>
+                                <td>Admin</td>
+                                <td><span class="status delivered">Active</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
 
                             <tr>
-                                <td>Apple Watch</td>
-                                <td>$1200</td>
-                                <td>Paid</td>
-                                <td><span class="status return">Return</span></td>
+                            <td>7</td>
+                                <td>Sasindu</td>
+                                <td>Admin</td>
+                                <td><span class="status delivered">Active</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
 
                             <tr>
-                                <td>Addidas Shoes</td>
-                                <td>$620</td>
-                                <td>Due</td>
-                                <td><span class="status inProgress">In Progress</span></td>
+                            <td>8</td>
+                                <td>Sasindu</td>
+                                <td>Admin</td>
+                                <td><span class="status delivered">Active</span></td>
+                                <td><ion-icon name="pencil-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></td>
                             </tr>
                         </tbody>
                     </table>
