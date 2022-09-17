337d8d102d0d8bbd37733ba43d366a820abd39a8
Sasidu Sandakalum <84926805+emusksasindu@users.noreply.github.com>
2022-09-18 00:59:25 +0530

    fix login list position

--- public/js/home/script.js
+++ public/js/home/script.js
@@ -18,21 +18,13 @@ document.querySelector('#close-search').onclick = () =>{
     searchForm.classList.remove('active');
 }
 
-window.onscroll = () =>{
-    navbar.classList.remove('active');
 
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.add('active');
-    }
-};
 
 window.onload = () =>{
     if(window.scrollY > 0){
         document.querySelector('.header').classList.add('active');
     }else{
-        document.querySelector('.header').classList.remove('active');
+        document.querySelector('.header').classList.add('active');
     }
 };
 
